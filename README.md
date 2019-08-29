54e845f8e5e1f413928e24ab2fd4b221b025bb84
Mike Carifio <mike@carif.io>
2019-08-29 09:15:41 -0400

    use /tmp as working directory instead of DENO_INSTALL

--- README.md
+++ README.md
@@ -6,13 +6,15 @@ Some bash and typescript scripts that "wrap" a deno installation and make it a l
 
 To use it, first "set it up":
 
-``bash
+```bash
 git clone git@github.com:mcarifio/deno-wrap
 sudo install --owner=${USER} --group=${USER} --directory /opt/deno
 ln --verbose --force deno-wrap/bin/deno-install-here.sh /opt/deno # make a hard link
 ```
 
-and then after that use it:
+Note that the example above is a compromise between sharing the executable across many users on a single system and making it easy to install without root. It also finesses all the various conventions surrounding "personal installs" on various platforms (windows, osx, linux).
+
+After the initial setup, run it in its location to install a deno, either the latest of a particular tag (version):
 
 ```bash
 /opt/deno/deno-install-here.sh
--- bin/deno-install-here.sh
+++ bin/deno-install-here.sh
@@ -17,26 +17,24 @@ me=$(realpath ${BASH_SOURCE})
 here=${me%/*}
 current=${1:-current}
 tag=${2}
-folder=${tag:-latest}
-DENO_INSTALL=${DENO_INSTALL:-${here}/${folder}}
+# folder=${tag:-latest}
+tmp=/tmp/${me##*/}/working-$$
+mkdir -p ${tmp}
 
 [[ ! -d ${DENO_INSTALL} ]] || mkdir -pv ${DENO_INSTALL}  # deno.land install.sh has different defaults
-printf "installing deno '${tag:-latest}' into '${DENO_INSTALL}'"
-curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=${DENO_INSTALL} sh -s ${tag}
-this_deno=${DENO_INSTALL}/bin/deno
+
+printf "installing deno '${tag:-latest}' into working folder '${tmp}' until we confirm it's version."
+curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=${tmp} sh -s ${tag}
+this_deno=${tmp}/bin/deno
 
 welcome=https://deno.land/welcome.ts
 ${this_deno} ${welcome} || error "'${this_deno}' couldn't run '${welcome}' successfully."
 this_version=$(${this_deno} --version | grep deno | cut -c7-)
 
-if [[ ${folder} = latest ]] ; then
-    # Renaming where deno is installed.
-    DENO_INSTALL=${here}/deno-${this_version}
-    # Set aside the old installation just in case.
-    [[ -d ${DENO_INSTALL} ]] && mv -vf ${DENO_INSTALL}{,-$$}
-    # Rename the installation location to include the deno version.
-    mv -v ${here}/latest ${DENO_INSTALL}
-fi
+deno_root=${here}/deno-${this_version}
+DENO_INSTALL=${DENO_INSTALL:-${deno_root}}
+[[ -d ${DENO_INSTALL} ]] && error "folder '${DENO_INSTALL}' exists, working deno left in folder '${tmp}'"
+mv -vf ${tmp} ${DENO_INSTALL}
 
 new_current=${here}/${current}
 [[ -d ${new_current} ]] && unlink ${new_current}
